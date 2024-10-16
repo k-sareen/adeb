df40ead0dd5a853723dcdfad6a8a3ac7955f6a2a
Kunal Sareen <kunal.sareen@anu.edu.au>
2024-10-16 01:01:57 +0000

    Uncomment the build-bpftrace.sh script

--- bpftrace/build-bpftrace.sh
+++ bpftrace/build-bpftrace.sh
@@ -4,10 +4,10 @@
 spath=$( cd "$(dirname "$0")" ; pwd -P )
 cd $spath
 
-# rm -rf build && mkdir -p build
-# cmake -B ./build -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DSTATIC_LINKING=ON
-# make -C ./build -j$(nproc)
-# cd build
-# make install
-# cd ../
-# rm -rf build
+rm -rf build && mkdir -p build
+cmake -B ./build -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DSTATIC_LINKING=ON
+make -C ./build -j$(nproc)
+cd build
+make install
+cd ../
+rm -rf build
